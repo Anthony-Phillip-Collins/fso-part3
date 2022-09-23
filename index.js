78c3aa0e8551f7435066b162b0097c741e3c6382
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-09-23 16:27:59 +1000

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const morgan = require('morgan');
 
 const app = express();
 
+morgan.token('payload', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(
+  morgan(':method :url :status :res[content-length] :response-time ms :payload')
+);
 
 let persons = [
   {
